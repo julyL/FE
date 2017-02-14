fbf01fe1c022d76db9bc465c124bd4c06de8b28c
julyL <849424162@qq.com>
2017-02-14 17:56:15 +0800

    promise

--- code/2-13/my-promise.js
+++ code/2-13/my-promise.js
@@ -1,50 +1,74 @@
 function p(resolver) {
      this.state='pending';
      this.tasklist=[];
+     this.selfpromise=this;
      var re=function(){p.resolve(this)}.bind(this),
          rj=function(){p.reject(this)}.bind(this);
      resolver(re,rj)
     
 }
+function isPromise(val){
+     return val instanceof p;
+}
+function run(tasklist){
 
+
+}
 p.prototype.then = function(fn) {
     if(this.state=="resolve"){   //同步的情况, promise已经resolve
-        fn();
+        var pro=fn();
+        if(isPromise(pro)){
+            this.selfpromise=pro;
+        } 
     }else{
         this.tasklist.push(fn);
     }
+    return this;
 }
 p.prototype.catch = function() {}
 
 p.resolve = function(pro) {
     pro.state="resolve";
-    var task=pro.tasklist.shift();
-    task&&task();
+    //  按tasklist队列执行task,
+    //  如果task返回promise对象，则将剩下未执行的tasklist作为这个promise对象resolve时的执行队列
+    
+    
 }
 
 p.reject = function() {}
 p.all = function() {}
 p.race = function() {}
 
-// 测试代码如下: 
+var P=new p((re,rj)=>{
+    setTimeout(()=>{
+        console.log("task1: wait 2000");
+        re();
+    },1000)
+}).then(()=>{
+      console.log("task1 is finish");
+      return new p((re,rj)=>{
+            setTimeout(()=>{
+                console.log("task2: wait 1000");
+                re();
+            },1000)
+      });
+}).then(()=>{
+    console.log("task2 is finish");
+})
 
-// new p((re,rj)=>{
-//     setTimeout(()=>{
+// 同步测试
+// var P=new p((re,rj)=>{
 //         console.log("task1: wait 2000");
 //         re();
-//     },1000)
 // }).then(()=>{
 //       console.log("task1 is finish");
 //       return new p((re,rj)=>{
-//             setTimeout(()=>{
 //                 console.log("task2: wait 1000");
 //                 re();
-//             },1000)
 //       });
 // }).then(()=>{
 //     console.log("task2 is finish");
 // })
-
 
 
 // new Promise((re,rj)=>{
