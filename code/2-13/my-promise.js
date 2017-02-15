52f4bc74c53a29926bdb61f0b1a871e0e4c25db2
julyL <849424162@qq.com>
2017-02-15 14:09:20 +0800

    修复同步执行问题

--- code/2-13/my-promise.js
+++ code/2-13/my-promise.js
@@ -29,40 +29,25 @@ p.prototype.then = function(fn) {
     return this;
 }
 p.resolve = function(pro) {
-    pro.state="resolve";
-    //  取出tasklist队列第一个task执行
-    //  如果task返回promise对象[A]，则将剩下未执行的tasklist作为[A]的执行队列
-    //  如果是同步代码,[A]会先执行，但是执行时的tasklist还未指定。解决：只能在resolve执行之前就指定tasklist
-    var task=pro.tasklist.shift(),
-        returndata;
-    if(task){
-        returndata=task();
-        if(isPromise(returndata)){
-            returndata.tasklist=pro.tasklist;
-        }else{
-            p.resolve(pro);
-        }
-    }  
-}
-// var P=new p((re,rj)=>{
-//     setTimeout(()=>{
-//         console.log("task1: wait 2000");
-//         re();
-//     },1000)
-// }).then(()=>{
-//       console.log("task1 is finish");
-//       return new p((re,rj)=>{
-//             setTimeout(()=>{
-//                 console.log("task2: wait 1000");
-//                 re();
-//             },1000)
-//       });
-// }).then(()=>{
-//     console.log("task2 is finish");
-// })
+    setTimeout(function(){
+        pro.state="resolve";
+        //  取出tasklist队列第一个task执行
+        //  如果task返回promise对象[A]，则将剩下未执行的tasklist作为[A]的执行队列
+        //  如果是同步代码,[A]会先执行，但是执行时的tasklist还未指定。解决办法:是外面嵌套一层setTimout,之前认为不能这么做，会打乱普通代码的执行顺序。但执行测试6,发现官方好像就是这么处理的
 
-//同步测试失败
-console.log('同步测试');
+        var task=pro.tasklist.shift(),
+            returndata;
+        if(task){
+            returndata=task();          //
+            if(isPromise(returndata)){         
+                returndata.tasklist=pro.tasklist;
+            }else{
+                p.resolve(pro);
+            }
+        }  
+    },0)
+}
+//测试1
 var P=new p((re,rj)=>{
     setTimeout(()=>{
         console.log("task1: wait 2000");
@@ -71,11 +56,39 @@ var P=new p((re,rj)=>{
 }).then(()=>{
       console.log("task1 is finish");
       return new p((re,rj)=>{
+            setTimeout(()=>{
                 console.log("task2: wait 1000");
                 re();
+            },1000)
       });
 }).then(()=>{
     console.log("task2 is finish");
 })
 
+//测试2
+// var P=new p((re,rj)=>{
+//     setTimeout(()=>{
+//         console.log("task1: wait 2000");
+//         re();
+//     },1000)
+// }).then(()=>{
+//       console.log("task1 is finish");
+//       return new p((re,rj)=>{
+//                 console.log("task2: wait 1000");
+//                 re();
+//       });
+// }).then(()=>{
+//     console.log("task2 is finish");
+// })
 
+
+//测试6
+// var a=new Promise((re)=>{
+//     console.log(1);
+//     re();
+// })
+// a.then(()=>{
+//     console.log(2);
+// })
+// console.log(3)
+//   1  3  2
