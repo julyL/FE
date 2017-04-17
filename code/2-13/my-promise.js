f8f254252697d1343d4447ee79ed333fa67e94cb
julyL <849424162@.com>
2017-04-18 00:09:16 +0800

    promise 4/18

--- code/2-13/my-promise.js
+++ code/2-13/my-promise.js
@@ -1,55 +1,47 @@
-p.reject = function() {}
-p.all = function() {}
-p.race = function() {}
-p.prototype.catch = function() {}
-function isPromise(val){
-     return val instanceof p;
+function isFunction(val){
+    return typeof val=='function';
 }
-function run(tasklist){
+function isObject(val){
+    return typeof val=='object';
+}
 
-}
-function p(resolver) {
+function pro(fn) {
      this.state='pending';
      this.value;
      this.tasklist=[];
-     this.selfpromise=this;
      var re=function(resolveData){this.resolve(resolveData)}.bind(this),
          rj=function(resolveData){this.reject(resolveData)}.bind(this);
-     resolver(re,rj)
-    
+         if(isFunction(fn)){
+             fn(re,rj) 
+         }
+     
 }
-p.prototype.then = function(fn) {
-    if(this.state=="resolve"){   //同步的情况, promise已经resolve
-        var pro=fn();
-        if(isPromise(pro)){
-            this.selfpromise=pro;
-        } 
-    }else{
-        this.tasklist.push(fn);
+pro.prototype.then = function(refn,rjfn) {
+    var returnValue;
+    if(this.status=='resolve'){
+        if(isFunction(refn)){
+           returnValue=setTimeout(refn(this.value));
+        }
     }
-    return this;
+    if(!isObject(returnValue)){
+        var p=new pro();
+        p.resolve(returnValue);
+        return p;
+    }
 }
-p.prototype.resolve = function(resolveData) {
-    setTimeout(function(){
-        this.state="resolve";
-        //  取出tasklist队列第一个task执行
-        //  如果task返回promise对象[A]，则将剩下未执行的tasklist作为[A]的执行队列
-        //  如果是同步代码,[A]会先执行，但是执行时的tasklist还未指定。解决办法:是外面嵌套一层setTimout,之前认为不能这么做，会打乱普通代码的执行顺序。但执行测试6,发现官方也是增加setTimeout
+pro.prototype.resolve = function(resolveData) {
+    if(this.status!="resolve"&&this.status!="reject"){
+        this.status="resolve";
+        this.value=resolveData;
+    }else{
+        
+    }
+}
+pro.prototype.reject = function(resolveData) {
 
-        var task=this.tasklist.shift(),
-            returndata;
-        if(task){
-            returndata=task(resolveData);          //
-            if(isPromise(returndata)){         
-                returndata.tasklist=this.tasklist;
-            }else{
-                this.resolve(resolveData);
-            }
-        }  
-    }.bind(this),0)
 }
 //测试0
-var P=new p((re,rj)=>{
+var P=new pro((re,rj)=>{
     setTimeout(()=>{
         console.log("task1: wait 2000");
         re("data1");
