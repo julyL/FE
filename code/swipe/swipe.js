e0f34fdbdc2de4a99e6a9f6a0e1ed65b9e2f995c
julyL <849424162@.com>
2017-05-14 17:39:16 +0800

    swipe beta

--- code/swipe/swipe.js
+++ code/swipe/swipe.js
@@ -5,36 +5,59 @@
     var Swipe = function(config) {
         this.container = config.container;
         this.child = config.child;
-        this.intervaltime = config.intervaltime || 1000;
-        var con = this.container;
-        var clientW = con.clientWidth,
-            activeIndex = 0,
+        this.intervaltime = config.intervaltime || 5000;
+        this.timer = null;
+        this.showIndicators = config.showIndicators === false ? false : true;
+        var con = this.container,
+            clientW = con.clientWidth,
             len = this.child.length,
-            limitWidth = 40;
-        this.child[activeIndex].style.display="block";
-        this.child[activeIndex].style.transform="translate3d(0px, 0px, 0px)";
+            self = this,
+            limitWidth = 40,
+            indicators,
+            indicator = [],
+            activeIndicator;
+        if (len == 1) {
+            return;
+        }
         var status = {
             X: 0,
             sX: 0,
             mX: 0,
             eX: 0,
+            activeIndex: 0,
             animating: false,
             prevElement: this.child[len - 1],
             nextElement: this.child[1],
-            activeElement:this.child[0],
-            moveDirection:"0",   //   -1:左  1:右 
+            activeElement: this.child[0],
+            moveDirection: "0", //   -1:左  1:右 
         }
-        this.status = status;
-        var self = this;
+        this.status=status;
+        self.container.classList.add("swipe-container");
+        this.child.forEach(function(el) {
+            el.classList.add("swipe-item");
+        })
+        this.child[status.activeIndex].style.display = "block";
+        this.child[status.activeIndex].style.transform = "translate3d(0px, 0px, 0px)";
+        if (this.showIndicators) { //显示计时器
+            indicators = document.createElement("div");
+            indicators.classList.add("swipe-indicators");
+            for (var i = 0; i < len; i++) {
+                indicator[i] = document.createElement("div");
+                indicator[i].classList.add("swipe-indicator");
+                if (i == status.activeIndex) {
+                    indicator[i].classList.add("is-active");
+                    activeIndicator = indicator[i];
+                }
+                indicators.appendChild(indicator[i]);
+            }
+            this.container.appendChild(indicators);
+        }
 
         function touchStart(e) {
-            if (status.animating) {
-                self.stopAnimate();
-            }
+            stopAnimate();
             status.X = self.getX(status.activeElement);
             self.stopTransition();
             status.sX = e.changedTouches[0].pageX;
-            console.log(1, status);
         }
 
         function setLocation(el, x) {
@@ -43,41 +66,39 @@
 
         function touchMove(e) {
             self.stopTransition();
-            console.log(2, status);
             status.mX = e.changedTouches[0].pageX;
             var changeX = status.X + status.mX - status.sX;
-            if (status.mX > status.sX) {   // moveDirection=1  右边
-                if(status.moveDirection==-1){   //  先左滑动 再右滑动 (当前activeIndex右边的位置可能出现错位，进行手动定位修复)
-                    self.fixLocation(activeIndex==len-1?0:activeIndex+1)
+            if (status.mX > status.sX) { // moveDirection=1  右边
+                if (status.moveDirection == -1) { //  先左滑动 再右滑动 (当前status.activeIndex右边的位置可能出现错位，进行手动定位修复)
+                    self.fixLocation(status.activeIndex == len - 1 ? 0 : status.activeIndex + 1)
                 }
-                status.moveDirection=1;
-                console.log(activeIndex,"->")
-                status.prevElement = self.child[activeIndex == 0 ? len - 1 : activeIndex - 1];
-                status.nextElement=
+                status.moveDirection = 1;
+                // console.log(status.activeIndex, "->")
+                status.prevElement = self.child[status.activeIndex == 0 ? len - 1 : status.activeIndex - 1];
                 status.prevElement.style.display = "block";
-                if (!status.animating) {   // 不是在切换动画过程中，则设置初始位置
-                    setLocation(status.prevElement,-clientW);
+                if (!status.animating) { // 不是在切换动画过程中，则设置初始位置
+                    setLocation(status.prevElement, -clientW);
                 }
-                setLocation(status.prevElement,changeX - clientW);
-            } else {    // moveDirection=-1  左边
-                if(status.moveDirection==1){  
-                    self.fixLocation(activeIndex==0?len-1:activeIndex-1)
+                setLocation(status.prevElement, changeX - clientW);
+            } else { // moveDirection=-1  左边
+                if (status.moveDirection == 1) {
+                    self.fixLocation(status.activeIndex == 0 ? len - 1 : status.activeIndex - 1)
                 }
-                status.moveDirection=-1;
-                console.log(activeIndex,"<-")
-                status.nextElement = self.child[activeIndex == len - 1 ? 0 : activeIndex + 1];
+                status.moveDirection = -1;
+                // console.log(status.activeIndex, "<-")
+                status.nextElement = self.child[status.activeIndex == len - 1 ? 0 : status.activeIndex + 1];
                 status.nextElement.style.display = "block";
                 if (!status.animating) {
-                    setLocation(status.nextElement,clientW);
+                    setLocation(status.nextElement, clientW);
                 }
-                setLocation(status.nextElement,changeX + clientW);
+                setLocation(status.nextElement, changeX + clientW);
             }
-            status.activeElement=self.child[activeIndex];
-            setLocation(status.activeElement,changeX );
+            status.activeElement = self.child[status.activeIndex];
+            setLocation(status.activeElement, changeX);
         }
 
         function touchEnd(e) {
-            status.moveDirection=0;
+            status.moveDirection = 0;
             self.setTransition();
             status.eX = e.changedTouches[0].pageX;
             var diff = status.eX - status.sX;
@@ -85,7 +106,7 @@
                 if (diff > limitWidth) {
                     setLocation(status.prevElement, 0);
                     setLocation(status.activeElement, clientW);
-                    activeIndex = activeIndex == 0 ? len - 1 : activeIndex - 1;
+                    status.activeIndex = status.activeIndex == 0 ? len - 1 : status.activeIndex - 1;
                 } else {
                     setLocation(status.prevElement, -clientW);
                     setLocation(status.activeElement, 0);
@@ -94,28 +115,71 @@
                 if (-diff > limitWidth) {
                     setLocation(status.nextElement, 0);
                     setLocation(status.activeElement, -clientW);
-                    activeIndex = activeIndex == len - 1 ? 0 : activeIndex + 1;
+                    status.activeIndex = status.activeIndex == len - 1 ? 0 : status.activeIndex + 1;
                 } else {
                     setLocation(status.nextElement, clientW);
                     setLocation(status.activeElement, 0);
                 }
             }
+            // debugger;
+            status.activeElement = self.child[status.activeIndex];
+            startAnimate();
+        }
+
+        function slide() {
+            status.nextElement = self.child[status.activeIndex == len - 1 ? 0 : status.activeIndex + 1];
+            self.stopTransition();
+            status.nextElement.style.display = 'block';
+            setLocation(status.nextElement, clientW);
+            setTimeout(function() {
+                self.setTransition();
+                setLocation(status.activeElement, -clientW);
+                setLocation(status.nextElement, 0);
+                status.activeIndex = (status.activeIndex + 1) % len;
+                status.activeElement = self.child[status.activeIndex];
+            },10)
+        }
+        window.slide = slide;
+
+        function stopAnimate() {
+            status.animating = false;
+            clearInterval(self.timer);
+        }
+
+        function startAnimate() {
+            status.animating = true;
+            self.timer = setInterval(function() {
+                slide();
+            }, self.intervaltime)
         }
 
         function initEvent() {
             self.addEvent(con, 'touchstart', touchStart);
             self.addEvent(con, 'touchmove', touchMove);
             self.addEvent(con, 'touchend', touchEnd);
+            self.child.forEach(function(el, number) {
+                self.addEvent(el, "transitionend", function() {
+                    if (el != status.activeElement) {
+                        el.style.display = "none";
+                    }
+                    if (self.showIndicators) {
+                        activeIndicator.classList.remove("is-active");
+                        activeIndicator = indicator[status.activeIndex];
+                        activeIndicator.classList.add("is-active");
+                    }
+                })
+            })
+            startAnimate();
         }
         initEvent();
-
     }
     Swipe.prototype = {
         addEvent: function(el, type, handle, iscapture) {
             el.addEventListener(type, handle, iscapture || false);
         },
         issupportCss3: function() {
-            return true;
+            var div = document.createElement("div");
+            return div.style.transform === undefined ? false : true;
         },
         stopTransition: function() {
             this.child.forEach(function(el) {
@@ -124,19 +188,21 @@
         },
         setTransition: function() {
             this.child.forEach(function(el) {
-                el.style.transitionDuration = '.4s';
+                el.style.transitionDuration = '.5s';
             })
         },
         getX: function(el) {
             var t = el.style.tranform;
             return /translate3d\((.*?),.*/g.exec(t) && /translate3d\((.*?),.*/g.exec(t)[0].slice(0, -2);
         },
-        fixLocation:function(index){
-            var el=this.child[index];
-            el.style.display="none";
-            el.style.transform="none";
+        fixLocation: function(index) {
+            var el = this.child[index];
+            el.style.display = "none";
+            el.style.transform = "none";
+        },
+        getIndex:function(){
+            return this.status.activeIndex;
         }
-
     }
     return Swipe;
 }));
