e65889f149d97820bda449de1989a5edb8c7cf3e
julyL <849424162@qq.com>
2017-06-12 18:29:52 +0800

    node

---
+++ code/Node/file.js
@@ -0,0 +1,38 @@
+var fs = require("fs");
+var path = require("path");
+
+
+function logSync(dir, callback) { //同步读取目录
+    fs.readdirSync(dir).forEach(function(file) {
+        var pathname = path.join(dir, file);
+        if (fs.statSync(pathname).isDirectory()) {
+            logSync(pathname, callback);
+        } else {
+            callback(pathname);
+        }
+    });
+}
+
+// logSync(__dirname,(str)=>{
+// 	console.log(str)
+// });
+
+
+// function log(dir, callback) { //异步读取
+//     fs.readdir(dir, (error, files) => {
+//     	files.forEach((file)=>{
+// 	        var pathname = path.join(dir, file);
+// 	        fs.stat(pathname, (error, data) => {
+// 	              console.log(data.isDirectory())
+// 	        })		
+//     	})
+//     })
+// }
+
+// log(__dirname, (str) => {
+//     console.log(str)
+// });
+
+fs.open(__dirname+"/test.txt",'a', '0666',(error,data)=>{
+		console.log(data)
+})
