4cb32e7d6c7684eec09da8f977bc7b210f0ed057
julyL <849424162@qq.com>
2017-09-28 23:47:13 +0800

    add safeSet

--- FE/Library/utils/src/safaSet.js
+++ FE/Library/utils/src/safaSet.js
@@ -14,25 +14,76 @@
  * var obj3=2;
  * safeSet(obj2,'1','wtf')  => {"1":'wtf'}
  * safeSet(obj2,'1','wtf',true)  => ["1":'wtf']
+ * 
+ * 思路:
+ * obj表示对象  k:表示key  result:需要赋值的值
+ * * 判断能否取值 obj    (代码A)
+ *      * 能取值
+ *              * obj[k]是否存在 
+ *                    * 存在       
+ *                          *  obj[k]不是引用对象   (代码C)
+ *                                * 是否是最后一个key
+ *                                      * 是,obj[k]=result
+ *                                      * 不是
+ *                                          *根据下一个key值和newArrayIfNeed  obj[k]={}或[] 重复A
+ *                          *  obj[k]是引用对象   下一个key?ob=obj[k]并重复A: obj[k]=result                 
+ *                    * 不存在
+ *                          *  是否是最后一个key
+ *                                      * 是,obj[k]=result
+ *                                      * 不是
+ *                                          *根据下一个key值和newArrayIfNeed  obj[k]={}或[] 重复A=result 
+ *      * 不能取值   (代码B)
+ *             * 根据key值和newArrayIfNeed  obj[k]={}或[]  下一个key?ob=obj[k]并重复A: obj[k]=result 
+ * 
  */
+function _newObjorArray(key, newArrayIfNeed) {
+  if (newArrayIfNeed && parseInt(key) == key && /^(([1-9]\d*)|0)$/.test(key)) {
+    return new Array(parseInt(key)); //  new Array("1") => ["1"]    -_-!!!
+  } else {
+    return {};
+  }
+}
 
-function safeSet(obj, path, val, newArrayIfNeed = false) {
+function safeSet(obj, path, result, newArrayIfNeed) {
   if (Array.isArray(path)) {
-    var ob;
-    for (var i = 0, len = path.length; i < len; i++) {
-      ob = ob[i];
-      if (typeof ob != "object" || ob == null || ob == undefined) {
-        // 无法取值的情况
-        if (toArray && parseInt(k) == k && /^(([1-9]\d*)|0)$/.test(k)) {
-          // k为整张数&&toArry,则初始化数组
-          ob = new Array(k);
+    var ob = obj,
+      ArrayObj = [];
+    for (var i = 0, len = path.length; i <= len - 1; i++) {
+      if (typeof ob == "object" && ob != null) {
+        ArrayObj.push(ob);
+        val = ob[path[i]];
+        if (val) {
+          if (typeof val == "object") {
+            //(代码C)
+            ob[path[i]] = val;
+            ob = ob[path[i]];
+          } else {
+            //
+            if (i == len - 1) {
+              ob[path[i]] = result;
+              ob = ob[path[i]];
+            } else {
+              ob[path[i]] = _newObjorArray(path[i + 1], newArrayIfNeed);
+              ob = ob[path[i]];
+            }
+          }
         } else {
-          // 初始化对象
-          ob = {};
+          if (i == len - 1) {
+            ob[path[i]] = result;
+            ob = ob[path[i]];
+          } else {
+            ob[path[i]] = _newObjorArray(path[i + 1], newArrayIfNeed);
+            ob = ob[path[i]];
+          }
         }
+      } else {
+        // (代码B)
+        ob = _newObjorArray(path[i], newArrayIfNeed);
+        ArrayObj.push(ob);
+        ob[path[i]] = val;
       }
     }
-    ob = val;
+    return ArrayObj[0];
   } else if (typeof path == "string") {
     var arrKeys = path.split("."),
       keys = [],
@@ -43,7 +94,19 @@ function safeSet(obj, path, val, newArrayIfNeed = false) {
         [].push.apply(keys, m);
       }
     });
-    return safeGet(obj, keys, val);
+    return safeSet(obj, keys, result, newArrayIfNeed);
   }
 }
+
+//var// obj1 = { a: 1 };
+// safeSet(obj1,'b.c[1]',2)
+// safeSet(obj1,'b.c[1]',2,true) // => {a:1,b:{c:[,2]}}
+
+//var obj2 = {};
+// safeSet(obj2,'1','wtf') // => {"1":'wtf'}
+// safeSet(obj2,'1','wtf',true) // => {1:'wtf'} // 只有当取的key值的父级(obj2)不为对象时,并且newArrayIfNeed==true 才会新建数组
+
+//var obj3 = 2;
+// safeSet(obj2,'1','wtf')  //=> {"1":'wtf'}
+//safeSet(obj3, "1", "wtf", true); //=> ["1":'wtf']
 export default safeSet;
